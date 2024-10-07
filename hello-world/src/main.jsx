90f76ea0d01c3cb04aa458d6d97ab8bc8018b6d2
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-07 08:52:37 -0700

    React 💪

---
+++ hello-world/src/main.jsx
@@ -0,0 +1,11 @@
+
+import { createRoot } from 'react-dom/client'
+
+function App(){
+  return <h1>React FrameWork 1</h1>
+}
+// import './index.css'
+
+createRoot(document.getElementById('root')).render(
+    <App /> 
+);
