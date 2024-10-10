6ea5d8ea598e4947b7a0bfb4e787be793be93d29
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-10 06:33:13 -0700

    React 💪

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,11 +1,11 @@
-
+import Chai from "./App"
 import { createRoot } from 'react-dom/client'
 
 function App(){
-  return <h1>React FrameWork 1</h1>
+  return (
+  <><h1>React FrameWork 1</h1><Chai /></>
+)
 }
-// import './index.css'
-
 createRoot(document.getElementById('root')).render(
     <App /> 
 );
