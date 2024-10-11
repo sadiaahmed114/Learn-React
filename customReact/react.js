db4fcf1c2c445ba328000db147a858375b920d00
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-11 12:39:43 -0700

    Create your own react library and jsx

--- customReact/react.js
+++ customReact/react.js
@@ -1,1 +1,22 @@
-const mainContainer = document.querySelector('#root')
+function customRender(reactElement , container){
+    const domElement = document.createElement
+    (reactElement.type)
+    domElement.innerHTML = reactElement.Children
+    domElement.setAttribute ('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+
+    container.appendChild(domElement)
+}
+
+const reactElement = {
+    type: 'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    Children:'Click me to visit google'
+}
+
+const mainContainer = document.querySelector('#root');
+
+customRender(reactElement,mainContainer)
