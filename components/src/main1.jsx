b24419489e21310154b238752c6a413dfb54ef19
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-10-30 05:07:08 -0700

    fb post

--- components/src/main1.jsx
+++ components/src/main1.jsx
@@ -60,7 +60,7 @@ function Post(Props) {
 const postData = [
   {
     name: "shehzad",
-    postImage: fruitImage,
+    // postImage: fruitImage,
     profilePhoto:
       "https://heroshotphotography.com/wp-content/uploads/2023/03/male-linkedin-corporate-headshot-on-white-square-1024x1024.jpg",
   },
