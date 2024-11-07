220aac2fcfcbf4fc77861a97baa076a1c81dc5a4
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-11-07 11:59:15 -0800

    react router

--- reactrouter/src/App.jsx
+++ reactrouter/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-800 p-4 '>React Router</h1>
+    <Header />
+    <Footer />
     </>
   )
 }
