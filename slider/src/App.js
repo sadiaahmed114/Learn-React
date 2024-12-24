35dc9129c6afe2914aed63004e0ca248b900421c
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-12-24 11:47:16 -0800

    slider

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,16 +1,16 @@
 function App(){
     return(
         <div>
-
+              <h1>Welcome to My Slider App</h1>
         </div>
 
     );
 }
 
-const data =[
-    {
-        name: `Saad`,
-        img:``,
-        review:`Lorem25`
-    },
-]
+// const data =[
+//     {
+//         name: `Saad`,
+//         img:``,
+//         review:`Lorem25`
+//     },
+// ]
