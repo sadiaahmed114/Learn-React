7d27cd664ffc0c1fd78ab7ff58bbaf0390498de2
Sadia Ahmed <sadiaahmed26373@gmail.com>
2024-12-28 10:17:20 -0800

    slider

--- project_slider/src/App.js
+++ project_slider/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-hero-section bg-cover bg-center h-screen">
+      <h1 className="text-white text-4xl font-bold">Hello World!</h1>
     </div>
   );
 }
--- project_slider/tailwind.config.js
+++ project_slider/tailwind.config.js
@@ -1,12 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero-section': "url('/images/th.jpeg')", // Double quotes aur sahi path ka istimaal karein
+      },
+    },
   },
   plugins: [],
-}
-
+};
